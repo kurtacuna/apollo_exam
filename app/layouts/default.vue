f0ce72becef561204a7caf7738af058ddc36c587
kurt <kurtlouvandrich.acuna@tup.edu.ph>
2026-03-14 00:42:42 +0800

    chore: remove old files

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,30 @@
+<script setup lang='ts'>
+
+</script>
+
 <template>
 	<v-app>
+		<v-app-bar 
+			elevation="0"
+			class="border-b"
+		>
+			<v-container>
+				<p>header</p>
+			</v-container>
+		</v-app-bar>
 		<v-main>
-			<slot />
+			<v-container>
+				<slot />
+			</v-container>
 		</v-main>
+		<v-footer class="border-t">
+			<v-container>
+				<p>footer</p>
+			</v-container>
+		</v-footer>
 	</v-app>
 </template>
-<script>
-export default {
-	name: 'DefaultLayout',
-}
-</script>
+
+<style scoped>
+
+</style>
