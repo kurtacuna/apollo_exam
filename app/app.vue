1e7eb0ba4f523c18840c853e1d0c8d9c5bf5d549
kurt <kurtlouvandrich.acuna@tup.edu.ph>
2026-03-14 00:35:10 +0800

    refactor: add nuxt 4 app.vue base

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <div>
+        <NuxtRouteAnnouncer></NuxtRouteAnnouncer>
+        <NuxtLayout>
+            <NuxtPage></NuxtPage>
+        </NuxtLayout>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
