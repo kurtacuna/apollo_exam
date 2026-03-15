483ba3ea8f25122306b90f3be05a1624677edbbd
kurt <kurtlouvandrich.acuna@tup.edu.ph>
2026-03-16 00:16:07 +0800

    feat: add favorite rockets page

---
+++ app/pages/rockets/favorites.vue
@@ -0,0 +1,38 @@
+<script setup lang='ts'>
+    const favoriteRocketsStore = useFavoriteRocketsStore()
+</script>
+
+<template>
+    <h1>Favorite Rockets</h1>
+    <p
+        v-if="!favoriteRocketsStore.favoriteRockets.length"
+        class="text-medium-emphasis"
+    >
+        No favorites added yet
+    </p>
+    <v-row size="2">
+        <v-col v-for="rocketId in favoriteRocketsStore.favoriteRockets" :key="rocketId" cols="1">
+            <v-card class="pa-8 text-center">
+                <h3 class="mt-0">Rocket ID: {{ rocketId }}</h3>
+                <v-btn
+                    variant="tonal"
+                    class="text-title-small w-auto me-4"
+                    color="red"
+                    @click="favoriteRocketsStore.removeFromFavorite(rocketId)"
+                >
+                    Remove
+                </v-btn>
+                <v-btn
+                    variant="outlined"
+                    :to="`/rockets/${rocketId}`"
+                >
+                    View Details
+                </v-btn>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<style scoped>
+
+</style>
