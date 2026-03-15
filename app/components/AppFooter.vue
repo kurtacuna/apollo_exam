10b445a9341947aad35db5aa2b1a4fd8d6d2e546
kurt <kurtlouvandrich.acuna@tup.edu.ph>
2026-03-15 21:42:08 +0800

    refactor: change footer content

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -3,43 +3,30 @@
 </script>
 
 <template>
-    <v-footer>
-        <v-container class="d-flex flex-column align-center justify-center">
+    <v-footer color="black pb-0">
+        <v-container class="pb-0 d-flex justify-space-between">
             <div class="mb-8">
                 <v-divider vertical thickness="5" style="height: 200px"></v-divider>
             </div>
-            <div class="d-flex ga-16">
-                <div class="text-center">
-                    <h6 class="text-display-large my-0">625</h6>
-                    <p class="my-0">
-                        COMPLETED
-                        <br>
-                        MISSIONS
-                    </p>
+            <div class="d-flex flex-column align-center">
+                <div class="mb-8 d-flex ga-16">
+                    <NuxtLink href="https://www.spacex.com/careers" target="_blank">Careers</NuxtLink>
+                    <NuxtLink href="https://www.spacex.com/updates" target="_blank">Updates</NuxtLink>
+                    <NuxtLink href="https://www.spacex.com/supplier" target="_blank">Suppliers</NuxtLink>
                 </div>
-                <div class="text-center">
-                    <h6 class="text-display-large my-0">585</h6>
-                    <p class="my-0">
-                        TOTAL
-                        <br>
-                        LANDINGS
-                    </p>
-                </div>
-                <div class="text-center">
-                    <h6 class="text-display-large my-0">548</h6>
-                    <p class="my-0">
-                        TOTAL
-                        <br>
-                        REFLIGHTS
-                    </p>
-                </div>
+                <v-img src="/Earth.svg" width="400" color="black"></v-img>
+            </div>
+            <div class="mb-8">
+                <v-divider vertical thickness="5" style="height: 200px"></v-divider>
             </div>
         </v-container>
     </v-footer>
 </template>
 
 <style scoped>
-    h6 {
-        font-size: 100px;
+    a {
+        text-decoration: none;
+        color: white;
+        font-size: 14px;
     }
 </style>
