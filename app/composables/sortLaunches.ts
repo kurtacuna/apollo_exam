4c933d3eb401df36d030957041c4f3bf3db68a95
kurt <kurtlouvandrich.acuna@tup.edu.ph>
2026-03-15 16:11:07 +0800

    feat: add sort launches

---
+++ app/composables/sortLaunches.ts
@@ -0,0 +1,35 @@
+import LaunchCardFragment from "~/fragments/LaunchCardFragment";
+import { useFragment, type FragmentType } from "~/gql";
+
+export const sortOptions = [
+    { title: "Ascending", value: "asc" },
+    { title: "Descending", value: "desc" },
+]
+
+export function useSortLaunches(launches: Ref<FragmentType<typeof LaunchCardFragment>[]>) {
+    const selectedSort = ref<string | undefined>(undefined)
+
+    const sortedLaunches = computed(() => {
+        const list = launches.value
+        if (!selectedSort.value) return list
+
+        const unmaskedLaunches = useFragment(LaunchCardFragment, list)
+        const maskedToUnmasked = list.map((launch, index) => {
+            return {
+                masked: launch,
+                unmasked: unmaskedLaunches[index]}
+        })
+
+        if (selectedSort.value == "asc") {
+            return maskedToUnmasked
+                .sort((a, b) => new Date(a.unmasked?.launch_date_local).getTime() - new Date(b.unmasked?.launch_date_local).getTime())
+                .map((obj) => obj.masked)
+        } else if (selectedSort.value == "desc") {
+            return maskedToUnmasked
+                .sort((a, b) => new Date(b.unmasked?.launch_date_local).getTime() - new Date(a.unmasked?.launch_date_local).getTime())
+                .map((obj) => obj.masked)
+        }
+    })
+
+    return { selectedSort, sortedLaunches }
+}
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,26 +19,40 @@ import LaunchCardFragment from '~/fragments/LaunchCardFragment'
 		const years = unmaskedLaunches.map((launch) => launch.launch_year).sort()
 		return [...new Set(years)]
 	})
+
+	const { selectedSort, sortedLaunches } = useSortLaunches(filteredLaunches)
 </script>
 
 <template>
 	<div class="d-flex justify-space-between align-end">
 		<h1>Launches</h1>
-		<v-select
-			v-model="selectedYear"
-			label="Filter by year"
-			:items="launchYears"
-			density="comfortable"
-			:hint="selectedYear && `Showing results for ${selectedYear}`"
-			persistent-hint
-			clearable
-			class="mb-1"
-			max-width="200",
-			:menu-props="{ scrim: true, scrollStrategy: 'close' }"
-		></v-select>
+		<div class="d-flex">
+			<v-select
+				v-model="selectedYear"
+				label="Filter by year"
+				:items="launchYears"
+				density="comfortable"
+				:hint="selectedYear && `Showing results for ${selectedYear}`"
+				persistent-hint
+				clearable
+				class="mb-1 me-2"
+				width="200",
+				:menu-props="{ scrim: true, scrollStrategy: 'close' }"
+			></v-select>
+			<v-select
+				v-model="selectedSort"
+				label="Sort launch date"
+				density="comfortable"
+				clearable
+				width="200"
+				class="mb-1"
+				:menu-props="{ scrim: true, scrollStrategy: 'close' }"
+				:items="sortOptions"
+			></v-select>
+		</div>
 	</div>
 	<v-row size="3">
-		<v-col v-for="(launch, index) in filteredLaunches" :key="index" cols="1">
+		<v-col v-for="(launch, index) in sortedLaunches" :key="index" cols="1">
 			<LaunchCard :launch="launch"></LaunchCard>
 		</v-col>
 	</v-row>
